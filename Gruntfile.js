882313c3aea3d42b7fc40a0253d765d911672dc1
tim.tang <tang.jilong@139.com>
2013-11-17 23:16:40 +0800

    [hearthstone] - Add code coverage and trais cov.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,34 @@
 'use strict';
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
     grunt.initConfig({
         // Configure a mochaTest task
         mochaTest: {
             test: {
                 options: {
-                    reporter: 'spec'
+                    reporter: 'spec',
+                    require: 'coverage/blanket'
                 },
                 src: ['test/**/*-test.js']
+            },
+            coverage: {
+                options: {
+                    reporter: 'html-cov',
+                    // use the quiet flag to suppress the mocha console output
+                    quiet: true,
+                    // specify a destination file to capture the mocha
+                    // output (the quiet option does not suppress this)
+                    captureFile: 'coverage.html'
+                },
+                src: ['test/**/*-test.js']
+            },
+            // The travis-cov reporter will fail the tests if the
+            // coverage falls below the threshold configured in package.json
+            'travis-cov': {
+                options: {
+                    reporter: 'travis-cov'
+                },
+                src: ['test/**/*.js']
             }
         },
         jshint: {
@@ -18,16 +38,16 @@ module.exports = function (grunt) {
             all: ['Gruntfile.js', '**/*.js', '!node_modules/**/*.js', '!test/**/*.js']
         },
         clean: {
-            dist: ['assets/themes/mark-reid/css/main.min.css', 'assets/themes/mark-reid/js/scripts.min.js']
+            dist: ['coverage.html']
         }
     });
 
     // Load tasks
-    //grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
 
     // Register tasks
-    grunt.registerTask('default', ['jshint', 'mochaTest']);
+    grunt.registerTask('default', ['clean', 'jshint', 'mochaTest']);
     //grunt.registerTask('dev', ['watch']);
 };

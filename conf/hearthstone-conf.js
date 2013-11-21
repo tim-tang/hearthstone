9883d45a8a18ae3e2c0ba33881ff430fb897a6ff
tim.tang <tang.jilong@139.com>
2013-11-21 20:31:34 +0800

    [hearthstone] [skip ci] - Prepare move configuration to js.

--- conf/hearthstone-conf.js
+++ conf/hearthstone-conf.js
@@ -5,7 +5,19 @@
  */
 
 exports.config = {
+    // MongoDB configure
     mongo: 'mongodb://127.0.0.1/hearthstone_dev',
+
+    // Session and Auth cookie configure
     session_secret: 'hearthstone_srv',
-    auth_cookie_name: 'hearthstone_srv'
+    auth_cookie_name: 'hearthstone_srv',
+
+    // Hearthstone server configure
+    dev_hearthstone_host: '127.0.0.1',
+    dev_hearthstone_port: 5000,
+    pro_hearthstone_host: 'http://hearthstone-srv.herokuapp.com',
+    pro_hearthstone_port: 80,
+
+    // Http header
+    content_type: 'application/json'
 };

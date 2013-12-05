e539ea967dd811708d88479162474ebf0ee06514
tim.tang <tang.jilong@139.com>
2013-12-05 18:56:55 +0800

    [hearthstone] - Refactor code.

--- model/index.js
+++ model/index.js
@@ -24,10 +24,8 @@ var userSchema = require('./user');
 var cardSchema = require('./card');
 var commentSchema = require('./comment');
 var versionScehma = require('./version');
-require('./ability');
 
 exports.User = mongoose.model('User',userSchema(mongoose));
 exports.Card = mongoose.model('Card', cardSchema(mongoose));
 exports.Comment = mongoose.model('Comment', commentSchema(mongoose));
 exports.Version = mongoose.model('Version', versionScehma(mongoose));
-exports.Ability= mongoose.model('Ability');

83ec431f3aac151944cf4dc17bf770529e6c1964
tim.tang <tang.jilong@139.com>
2013-12-06 21:32:52 +0800

    [hearthstone] - Use cluster to start hearthstone server.

--- hearthstone.js
+++ hearthstone.js
@@ -6,4 +6,21 @@
 
 "use strict";
 require('newrelic');
-var restfulServer = require('./server/restfulServer');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
+console.log("CPU Numbers ** %s", numCPUs);
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Hearthstone worker ' + worker.process.pid + ' died');
+    });
+} else {
+    // Workers can share any TCP connection
+    // In this case its a HTTP server
+    var restfulServer = require('./server/restfulServer');
+}

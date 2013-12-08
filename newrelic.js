e34d608ec3ba17bc4db9b5f8757c8617e50f96e1
tim.tang <tang.jilong@139.com>
2013-12-08 14:33:24 +0800

    [hearthstone] [skip ci] - Update comments.

--- newrelic.js
+++ newrelic.js
@@ -5,20 +5,16 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
-  /**
-   * Array of application names.
-   */
+
+  //Array of application names.
   app_name : ['hearthstone_srv'],
-  /**
-   * Your New Relic license key.
-   */
+
+  //Your New Relic license key.
   license_key : '84264dd9f7fa8b72c7178d8fab501c22695d9e93',
   logging : {
-    /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
-     */
+     //Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     //issues with the agent, 'info' and higher will impose the least overhead on
+     //production applications.
     level : 'info'
   }
 };
